0a563ec115ae54441be311be5bcabc7ba8a1e561
Harrizon <harryalex0281@hotmail.com>
2025-12-05 23:33:30 -0500

    fix: exclude abstract inventory test from running

--- tests/interfaces/repositories/test_inventory_repo.py
+++ tests/interfaces/repositories/test_inventory_repo.py
@@ -2,56 +2,55 @@ import unittest
 from interfaces.repositories.inventory_repo import InventoryRepository
 from demo.mocks.products import MOCKS_PRODUCTS
 
-class TestInventoryRepository(unittest.TestCase):
-    repository: InventoryRepository
+class NotTested:
+    class TestInventoryRepository(unittest.TestCase):
+        repository: InventoryRepository
 
-    def tearDown(self):
-        self.repository.dispose()
-        self.repository = None
+        def tearDown(self):
+            self.repository.dispose()
+            self.repository = None
 
-    def test_add_product(self):
-        expected = MOCKS_PRODUCTS[0]
+        def test_add_product(self):
+            expected = MOCKS_PRODUCTS[0]
 
-        self.assertNotIn(expected, self.repository.list_products(), "product to add is already in the list")
+            self.assertNotIn(expected, self.repository.list_products(), "product to add is already in the list")
 
-        self.repository.add_product(expected)
-        actual = self.repository.get_product(expected.id)
+            self.repository.add_product(expected)
+            actual = self.repository.get_product(expected.id)
 
-        # Check attributes
-        self.assertEqual(expected.id, actual.id, "incorrect product id")
-        self.assertEqual(expected.name, actual.name, "incorrect product name")
-        self.assertEqual(expected.price, actual.price, "incorrect product price")
-        self.assertEqual(expected.category, actual.category, "incorrect product category")
+            # Check attributes
+            self.assertEqual(expected.id, actual.id, "incorrect product id")
+            self.assertEqual(expected.name, actual.name, "incorrect product name")
+            self.assertEqual(expected.price, actual.price, "incorrect product price")
+            self.assertEqual(expected.category, actual.category, "incorrect product category")
 
-        # Is created product in the list?
-        self.assertIn(expected, self.repository.list_products(), "expected product is not in the list")
+            # Is created product in the list?
+            self.assertIn(expected, self.repository.list_products(), "expected product is not in the list")
 
-    def test_remove_product(self):
-        expected = MOCKS_PRODUCTS[1]
+        def test_remove_product(self):
+            expected = MOCKS_PRODUCTS[1]
 
-        self.assertNotIn(expected, self.repository.list_products(), "product to remove is already in the list")
+            self.assertNotIn(expected, self.repository.list_products(), "product to remove is already in the list")
 
-        self.repository.add_product(expected)
-
-        self.assertIn(expected, self.repository.list_products(), "expected product is not in the list")
-
-        isRemoved = self.repository.remove_product(expected.id)
-
-        self.assertTrue(isRemoved, "failed to remove product")
-
-        self.assertNotIn(expected, self.repository.list_products(), "removed product is still in the list")
-
-        self.assertIsNone(self.repository.get_product(expected.id), "removed product is still indexable")
-    def test_add_existing_product_id_raises_error(self):
-        existing = MOCKS_PRODUCTS[2]
-        self.repository.add_product(existing)
-
-        expected = MOCKS_PRODUCTS[4]
-
-        # Have the same ID to raise error
-        expected.id = existing.id
-
-        with self.assertRaises(ValueError):
             self.repository.add_product(expected)
 
-        
+            self.assertIn(expected, self.repository.list_products(), "expected product is not in the list")
+
+            isRemoved = self.repository.remove_product(expected.id)
+
+            self.assertTrue(isRemoved, "failed to remove product")
+
+            self.assertNotIn(expected, self.repository.list_products(), "removed product is still in the list")
+
+            self.assertIsNone(self.repository.get_product(expected.id), "removed product is still indexable")
+        def test_add_existing_product_id_raises_error(self):
+            existing = MOCKS_PRODUCTS[2]
+            self.repository.add_product(existing)
+
+            expected = MOCKS_PRODUCTS[4]
+
+            # Have the same ID to raise error
+            expected.id = existing.id
+
+            with self.assertRaises(ValueError):
+                self.repository.add_product(expected)
